0ee10ee5e878e86b389e12a301dac5d24a8d48e3
gelea <hoa.vanthanh@hotmail.de>
2021-07-14 01:51:35 +0200

    add gh-pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './sayHello.js';
 import sayHello from './sayHello.js';
 
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
 function addToDom(text) {
     let h3 = document.createElement('h3');
     h3.innerText = text;
@@ -9,3 +14,7 @@ function addToDom(text) {
 
 addToDom('Something added to the DOM');
 sayHello('Hola, ¿qué tal?');
+
+$(document).ready(()=>{
+    $('body').html('Hello World, if you see this message Parcel Bundler and Github Hosting are working nicely!');
+})
